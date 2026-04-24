be1f99b9c696ffa8edc63750e252b5016af0ccf3
Ben Butler-Cole <ben@bridesmere.com>
2026-04-24 14:12:03 +0100

    add tests for ticket office service

--- ticket_office/test_ticket_office.py
+++ ticket_office/test_ticket_office.py
@@ -1,7 +1,52 @@
 from ticket_office import TicketOffice
+import responses
 
 
-def test_reserve_seats():
+def populate_train_data_service(count=None, numbers=None):
+    assert not (count and numbers)
+    if not numbers:
+        if not count:
+            count = 100
+        numbers = [str(s) for s in range(count)]
+    
+    seat_data = {}
+    for n in numbers:
+        seat_data[n] = {}
+
+    return responses.get("http://localhost:8081/data_for_train/express_2000", json={"seats": seat_data})
+
+@responses.activate
+def test_reserve_seats_on_correct_train():
+    populate_train_data_service()
+
     office = TicketOffice()
     result = office.reserve("express_2000", 4)
     assert result["train_id"] == "express_2000"
+
+@responses.activate
+def test_reserve_correct_number_of_seats_when_enough_seats_are_available():
+    populate_train_data_service(count=10)
+
+    office = TicketOffice()
+    result = office.reserve("express_2000", 4)
+    assert len(result["seats"]) == 4
+
+@responses.activate
+def test_reserves_no_seats_when_not_enough_seats():
+    populate_train_data_service(count=2)
+
+    office = TicketOffice()
+    result = office.reserve("express_2000", 4)
+    assert result['seats'] == []
+
+@responses.activate
+def test_check_seats_are_from_data_service():
+    populate_train_data_service(numbers=["1A", "2A"])
+
+    office = TicketOffice()
+    result = office.reserve("express_2000", 2)
+
+    assert set(result["seats"]) == {"1A","2A"}
+
+def test_correct_train_id_passed_to_other_service():
+    assert False
--- ticket_office/ticket_office.py
+++ ticket_office/ticket_office.py
@@ -1,11 +1,19 @@
 from flask import Flask, jsonify, request
+import requests
 
 app = Flask(__name__)
 
 
 class TicketOffice:
     def reserve(self, train_id, seat_count):
-        return {}
+        response = requests.get("http://localhost:8081/data_for_train/express_2000")
+        seats = response.json()
+
+        seat_numbers = list(seats["seats"].keys())[:seat_count]
+        if len(seat_numbers) < seat_count:
+            seat_numbers = []
+
+        return {"train_id":train_id, "seats":seat_numbers}
 
 
 service = None
@@ -19,12 +27,10 @@ def healthcheck():
 @app.route('/reserve', methods=["POST"])
 def reserve():
     train_id = request.form["train_id"]
-    seat_count = request.form["seat_count"]
+    seat_count = int(request.form["seat_count"])
     return jsonify(service.reserve(train_id, seat_count))
 
 
 if __name__ == "__main__":
-    import logging
-    logging.getLogger("werkzeug").setLevel(logging.WARNING)
     service = TicketOffice()
-    app.run(port=8083)
+    app.run(port=8083, debug=True)
