9284ce8db836d549e3f095502b049e07b8bc9132
Ben Butler-Cole <ben@bridesmere.com>
2026-04-24 15:08:56 +0100

    check booking reference service

--- ticket_office/test_ticket_office.py
+++ ticket_office/test_ticket_office.py
@@ -15,9 +15,15 @@ def populate_train_data_service(count=None, numbers=None, train_id="express_2000
 
     return responses.get(f"http://localhost:8081/data_for_train/{train_id}", json={"seats": seat_data})
 
+def populate_booking_reference_service(reference="a5123"):
+
+    return responses.get("http://localhost:8082/booking_reference", reference)
+
+
 @responses.activate
 def test_reserve_seats_on_correct_train():
     populate_train_data_service(train_id="slow_2000")
+    populate_booking_reference_service()
 
     office = TicketOffice()
     result = office.reserve("slow_2000", 4)
@@ -26,6 +32,7 @@ def test_reserve_seats_on_correct_train():
 @responses.activate
 def test_reserve_correct_number_of_seats_when_enough_seats_are_available():
     populate_train_data_service(count=10)
+    populate_booking_reference_service()
 
     office = TicketOffice()
     result = office.reserve("express_2000", 4)
@@ -34,6 +41,7 @@ def test_reserve_correct_number_of_seats_when_enough_seats_are_available():
 @responses.activate
 def test_reserves_no_seats_when_not_enough_seats():
     populate_train_data_service(count=2)
+    populate_booking_reference_service()
 
     office = TicketOffice()
     result = office.reserve("express_2000", 4)
@@ -42,6 +50,7 @@ def test_reserves_no_seats_when_not_enough_seats():
 @responses.activate
 def test_check_seats_are_from_data_service():
     populate_train_data_service(numbers=["1A", "2A"])
+    populate_booking_reference_service()
 
     office = TicketOffice()
     result = office.reserve("express_2000", 2)
@@ -51,6 +60,17 @@ def test_check_seats_are_from_data_service():
 @responses.activate
 def test_correct_train_id_passed_to_other_service():
     populate_train_data_service(train_id="slow_2000")
+    populate_booking_reference_service()
     office = TicketOffice()
     office.reserve("slow_2000", 4)
+    
+@responses.activate
+def test_includes_booking_reference():
+    populate_train_data_service()
+    booking_ref = populate_booking_reference_service(reference="b567")
+
+    office = TicketOffice()
+    result = office.reserve("express_2000", 4)
+    assert result['booking_reference'] == "b567"
+
     
--- ticket_office/ticket_office.py
+++ ticket_office/ticket_office.py
@@ -7,13 +7,14 @@ app = Flask(__name__)
 class TicketOffice:
     def reserve(self, train_id, seat_count):
         response = requests.get(f"http://localhost:8081/data_for_train/{train_id}")
+        booking_ref_response = requests.get("http://localhost:8082/booking_reference")
         seats = response.json()
 
         seat_numbers = list(seats["seats"].keys())[:seat_count]
         if len(seat_numbers) < seat_count:
             seat_numbers = []
 
-        return {"train_id":train_id, "seats":seat_numbers}
+        return {"train_id":train_id, "seats":seat_numbers, "booking_reference": booking_ref_response.text}
 
 
 service = None
